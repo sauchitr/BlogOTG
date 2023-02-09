825c6edef2b76a28c2717b017a9396dfd71af8e1
Sauchitr <ssp278@gmail.com>
2023-02-09 13:15:39 +0530

    Added automatically profile creation when user registers, added profile picture and other features

--- BlogOTG/urls.py
+++ BlogOTG/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from users import views as user_views
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path("", include("users.urls")),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
