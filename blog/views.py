b52ca3df0954409116baeb67b97dea00d99cd1b1
Sauchitr <ssp278@gmail.com>
2023-02-10 14:15:12 +0530

    Added CRUD functionality with direct links from Frontend. Used Class based views for the first time.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,67 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html" # default naming convention: appname/modelname_viewtype.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    """ not using the default instance name which is object
+      but keeping it to post ast it is more convenient."""
+    
+    context_object_name = "post" 
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    context_object_name = "post"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    context_object_name = "post"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    context_object_name = 'post'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
